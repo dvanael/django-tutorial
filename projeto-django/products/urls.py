afabb04ce8635d4d945d8de320fc3dea11d3cb8d
Anael Barbosa <anael.noki@gmail.com>
2024-01-18 21:59:30 -0300

    create crud.md

--- projeto-django/products/urls.py
+++ projeto-django/products/urls.py
@@ -4,4 +4,9 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('sobre/', about, name='about'),
+    path('Categoria/', product_list, name='product-list'),
+    path('produtos/cadastrar/', product_create, name='product-create'),
+    path('produtos/<int:pk>/', product_detail, name='product-detail'),
+    path('produtos/<int:pk>/atualizar/', product_update, name='product-update'),
+    path('produtos/<int:pk>/deletar/', product_delete, name='product-delete'),
 ]
--- projeto-django/products/views.py
+++ projeto-django/products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def index(request):
@@ -6,3 +8,39 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'products/product_detail.html', {'product': product})
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'products/product_form.html', {'form': form, 'action': 'Create'})
+
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'products/product_form.html', {'form': form, 'action': 'Update'})
+
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'products/product_confirm_delete.html', {'product': product})
