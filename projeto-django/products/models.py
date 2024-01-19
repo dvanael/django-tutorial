71152a9ad2fb4b8993f1aa535aa5d75cda4c2ae1
Anael Barbosa <joaoanael10@gmail.com>
2024-01-18 21:47:38 -0300

    create models.md

--- projeto-django/products/models.py
+++ projeto-django/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    categories = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
