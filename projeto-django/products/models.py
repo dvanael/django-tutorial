8b2b7c33978bf769830324a2a7a9bf717d80bfcf
Anael Barbosa <anael.noki@gmail.com>
2024-01-19 23:19:07 -0300

    create product_list.html

--- projeto-django/products/models.py
+++ projeto-django/products/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     description = models.TextField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.PositiveIntegerField(default=0)
-    categories = models.ForeignKey(Category, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
