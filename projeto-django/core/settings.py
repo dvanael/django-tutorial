e12b0c829a4de9105148c2be45df3ddc116f8c2c
Anael Barbosa <joaoanael10@gmail.com>
2024-01-20 17:21:15 -0300

    update templates and views.py

--- projeto-django/core/settings.py
+++ projeto-django/core/settings.py
@@ -28,7 +28,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
