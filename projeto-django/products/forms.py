e94e88cdeadb6350f202d36e2c28dd606e619dcf
Anael Barbosa <joaoanael10@gmail.com>
2024-01-19 23:19:07 -0300

    create product_list.html

--- projeto-django/products/forms.py
+++ projeto-django/products/forms.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price', 'stock_quantity', 'categories']
+        fields = ['name', 'description', 'price', 'stock_quantity', 'category']
--- projeto-django/products/urls.py
+++ projeto-django/products/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('sobre/', about, name='about'),
-    path('Categoria/', product_list, name='product-list'),
+    path('produtos/', product_list, name='product-list'),
     path('produtos/cadastrar/', product_create, name='product-create'),
     path('produtos/<int:pk>/', product_detail, name='product-detail'),
     path('produtos/<int:pk>/atualizar/', product_update, name='product-update'),
