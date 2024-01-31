9830f64aa3ea76441b08ca8e92b9d5d3838a7548
Anael Barbosa <joaoanael10@gmail.com>
2024-01-31 17:29:48 -0300

    update autenticate.md

--- projeto-django/products/views.py
+++ projeto-django/products/views.py
@@ -1,14 +1,64 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
-from django.contrib.auth.decorators import user_passes_test
-# from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group 
 from django.contrib.auth import authenticate, login
 
+from .utils import *
 from .models import Product
 from .forms import ProductForm, UserForm
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+@login_required
+def product_list(request):
+    products = get_admin(request, Product)
+    return render(request, 'products/product-list.html', {'products': products})
+
+@login_required
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'products/product-detail.html', {'product': product})
+
+@login_required
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.instance.user = request.user
+            form.save()
+            return redirect('product-list')
+    else:
+        form = ProductForm()
+    context = {'form': form, 'title': 'Cadastrar Produto'}
+    return render(request, 'form.html', context)
+
+@login_required
+def product_update(request, pk):
+    product = post_admin(request, Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product-list')
+    else:
+        form = ProductForm(instance=product)
+    context = {'form': form, 'title': 'Atualizar Produto'}
+    return render(request, 'form.html', context)
+
+@login_required
+def product_delete(request, pk):
+    product = post_admin(request, Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product-list')
+    context = {'object': product, 'title': 'Excluir Produto'}
+    return render(request, 'form-delete.html', context)
+    
 def register(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
@@ -25,61 +75,3 @@ def register(request):
     else:
         form = UserForm()
     return render(request, 'registration/register.html', {'form': form})
-
-def group_required(*group_name):
-    def in_group(user):
-        if user.groups.filter(name__in=group_name).exists():
-            return True
-        return False
-    return user_passes_test(in_group, login_url='/login/')
-
-def index(request):
-    return render(request, 'index.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-@group_required('user','admin')
-def product_list(request):
-    products = Product.objects.filter(user=request.user)
-    return render(request, 'products/product-list.html', {'products': products})
-
-@group_required('user','admin')
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'products/product-detail.html', {'product': product})
-
-@group_required('user','admin')
-def product_create(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            form.instance.user = request.user
-            form.save()
-            return redirect('product-list')
-    else:
-        form = ProductForm()
-    context = {'form': form, 'title': 'Cadastrar Produto'}
-    return render(request, 'form.html', context)
-
-@group_required('user','admin')
-def product_update(request, pk):
-    product = get_object_or_404(Product, pk=pk, user=request.user)
-    if request.method == 'POST':
-        form = ProductForm(request.POST, instance=product)
-        if form.is_valid():
-            form.save()
-            return redirect('product-list')
-    else:
-        form = ProductForm(instance=product)
-    context = {'form': form, 'title': 'Atualizar Produto'}
-    return render(request, 'form.html', context)
-
-@group_required('user','admin')
-def product_delete(request, pk):
-    product = get_object_or_404(Product, pk=pk, user=request.user)
-    if request.method == 'POST':
-        product.delete()
-        return redirect('product-list')
-    context = {'object': product, 'title': 'Excluir Produto'}
-    return render(request, 'form-delete.html', context)
